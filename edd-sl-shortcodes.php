5ca2056d3cef84f4eb82a21a2c0e12e3ae58ea15
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-03-20 21:47:11 +0100

    v1.0.0

--- edd-sl-shortcodes.php
+++ edd-sl-shortcodes.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: Easy Digital Downloads - Recurring Dashboard Widget
- * Description: Adds more useful information about your recurring income to EDD's dashboard widget.
+ * Plugin Name: Easy Digital Downloads - Software Licensing Shortcodes
+ * Description: Add additional shortcodes to Software Licensing.
  * Version: 1.0.0
  * Author: Daan from Daan.dev
  * Author URI: https://daan.dev
- * GitHub Plugin URI: Dan0sz/edd-recurring-dashboard-widget
+ * GitHub Plugin URI: Dan0sz/edd-sl-shortcodes
  * Primary Branch: master
  * License: MIT
  */
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$daan_edd_recurring_widget = new \Daan\EDD\Recurring\Widget();
+$daan_edd_sl_shortcodes = new \Daan\EDD\SoftwareLicensing\Widget();
