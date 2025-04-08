2d4d4533665f2702676b572550c01982755947c6
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-08 14:16:03 +0200

    Fixed: errors in checkout.

--- assets/js/view-changelog.js
+++ assets/js/view-changelog.js
@@ -2,25 +2,33 @@
  * Handles the display state of the View Changelog link.
  */
 document.addEventListener('DOMContentLoaded', function () {
+    const changelog = document.getElementById('daan-edd-sl-changelog');
+
     /**
      * Display changelog.
      */
-    document.getElementById('edd-sl-view-changelog').addEventListener('click', function () {
-        let changelog = document.getElementById('daan-edd-sl-changelog');
+    if (changelog !== null) {
+        changelog.addEventListener('click', function () {
+            let changelog = document.getElementById('daan-edd-sl-changelog');
 
-        if (changelog.style.display === 'none') {
-            changelog.style.display = 'block';
-        } else {
-            changelog.style.display = 'none';
-        }
-    });
+            if (changelog.style.display === 'none') {
+                changelog.style.display = 'block';
+            } else {
+                changelog.style.display = 'none';
+            }
+        });
+    }
+
+    const changelog_close = document.getElementById('daan-edd-sl-changelog-close');
 
     /**
      * Hide changelog.
      */
-    document.getElementById('daan-edd-sl-changelog-close').addEventListener('click', function () {
-        let changelog = document.getElementById('daan-edd-sl-changelog');
+    if (changelog_close !== null) {
+        changelog_close.addEventListener('click', function () {
+            let changelog = document.getElementById('daan-edd-sl-changelog');
 
-        changelog.style.display = 'none';
-    });
+            changelog.style.display = 'none';
+        });
+    }
 });
