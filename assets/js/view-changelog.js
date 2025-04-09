86c8b3d19e2240a1f19a2964d276db539508498b
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2025-04-09 15:23:13 +0200

    Fixed: display changelog.

--- assets/js/view-changelog.js
+++ assets/js/view-changelog.js
@@ -4,26 +4,28 @@
 document.addEventListener('DOMContentLoaded', function () {
     const changelog = document.getElementById('daan-edd-sl-changelog');
 
+    if (changelog === null) {
+        return;
+    }
+
     /**
      * Display changelog.
      */
-    if (changelog !== null) {
-        changelog.addEventListener('click', function () {
-            let changelog = document.getElementById('daan-edd-sl-changelog');
+    const changelog_link = document.getElementById('edd-sl-view-changelog');
 
-            if (changelog.style.display === 'none') {
-                changelog.style.display = 'block';
-            } else {
-                changelog.style.display = 'none';
-            }
-        });
-    }
-
-    const changelog_close = document.getElementById('daan-edd-sl-changelog-close');
-
+    changelog_link.addEventListener('click', function () {
+        if (changelog.style.display === 'none') {
+            changelog.style.display = 'block';
+        } else {
+            changelog.style.display = 'none';
+        }
+    });
+    
     /**
      * Hide changelog.
      */
+    const changelog_close = document.getElementById('daan-edd-sl-changelog-close');
+
     if (changelog_close !== null) {
         changelog_close.addEventListener('click', function () {
             let changelog = document.getElementById('daan-edd-sl-changelog');
